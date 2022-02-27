905d4d2afc3f325ffd036c3ca442b561861446e9
AshirMohab <amohabir@jhb.dvt.co.za>
2022-02-27 21:16:01 +0200

    style: :art: Minor changes to the fonts of the headings

--- reactCss/src/App.tsx
+++ reactCss/src/App.tsx
@@ -13,14 +13,14 @@ function App() {
       >
         <div className="font-bold text-3xl ml-4 justify-center xl:flex-auto xl:justify-start min-w-[100px]">
           <div className="w-2 h-2 rounded-full bg-primary"></div>
-          <div id="Logo" className="font-PT-serif -m-1.5">
+          <div id="Logo" className="font-serif -m-1.5">
             IT-tech
           </div>
         </div>
         <Header tags={["Home", "About", "Services", "Team"]}></Header>
       </div>
       <div id="Home" className="h-[100%] m-6">
-        <h1 className="flex h-[20%] lg:h-[30%] pt-10 font-poppins font-bold leading-relaxed text-[40px] text-header justify-end text-center md:text-left w-[100%] md:w-[50vh]">
+        <h1 className="flex h-[20%] lg:h-[30%] pt-10  font-bold leading-relaxed text-[40px] text-header justify-end text-center md:text-left w-[100%] md:w-[50vh]">
           Simplify your company systems with our IT Solutions
         </h1>
         <div className="grid md:grid-cols-2">
@@ -30,7 +30,7 @@ function App() {
           >
             <img src="./images/idea.jpg" className="rounded-lg" />
           </div>
-          <div className="flex flex-col mb-16 justify-betweenfont-bold">
+          <div className="flex flex-col mb-16 justify-between font-bold">
             <HelpCard
               helpHeadings={[
                 "Project Discussion",
@@ -43,7 +43,7 @@ function App() {
       </div>
       <div id="Services" className="bg-slate-50 p-6">
         <div id="ServicesHeader" className="p-6">
-          <h1 className="flex mt-44 font-poppins font-bold text-[40px] text-header text-center justify-center">
+          <h1 className="flex mt-44 font-bold text-[40px] text-header text-center justify-center">
             Here are the services we provide
           </h1>
           <p className="font-medium text-secondary text-sm text-center pt-2">
